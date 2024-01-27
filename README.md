5a01222795180d68cd631556f10dc33c86b521ab
Nicolás Hatcher <nicolas@theuniverse.today>
2024-01-27 23:06:24 +0100

    UPDATE: <3

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# Hats(!)
+# Hats
 
-Hats is a game to 
+This is the code accompanying the blog post in:
+
+https://www.nhatcher.com/post/on-hats-and-sats/
+
+
+# Thanks
+
+I used the [coloris](https://github.com/mdbassit/Coloris) color picker because the default used by the browser doesn't have an alpha channel.
+
--- sudoku.js
+++ sudoku.js
@@ -1,43 +1,43 @@
-// https://github.com/crillab/gophersat/blob/master/examples/sat-for-noobs.md
-
-// the statement there is a number n in (row, column) has index:
-// index = row*9*9+column*9+n
+// Generates all the clauses for an empty sudoku board
 const createClauses = () => {
     let clausesCount = 0;
-    const variableCount = 8 * 9 * 9 + 8 * 9 + 9; // 729
+    // 729 variables
+    const variableCount = 8 * 9 * 9 + 8 * 9 + 9;
     const data = [];
-    // Every row must contain every number
+
+    // Every row must contain every number (81 clauses)
     for (let row = 0; row < 9; row++) {
         for (let n = 1; n < 10; n++) {
             const statement = [];
             for (let column = 0; column < 9; column++) {
-                statement.push(row * 9 * 9 + column * 9 + n);
+                statement.push(fromCellToIndex(row, column, n));
             }
             clausesCount++;
             data.push(9, ...statement);
         }
     }
-    // every column must contain every number
+
+    // every column must contain every number (81 clauses)
     for (let column = 0; column < 9; column++) {
         for (let n = 1; n < 10; n++) {
             const statement = [];
             for (let row = 0; row < 9; row++) {
-                statement.push(row * 9 * 9 + column * 9 + n);
+                statement.push(fromCellToIndex(row, column, n));
             }
             clausesCount++;
             data.push(9, ...statement);
         }
     }
-    // every block must contain every number
+    // every block must contain every number (81 clauses)
     for (let blockX = 0; blockX < 3; blockX++) {
         for (let blockY = 0; blockY < 3; blockY++) {
             for (let n = 1; n < 10; n++) {
                 const statement = [];
                 for (let i = 0; i < 3; i++) {
-                    const row = blockX*3 + i;
+                    const row = blockX * 3 + i;
                     for (let j = 0; j < 3; j++) {
-                        const column = blockY*3+j;
-                        statement.push(row * 9 * 9 + column * 9 + n);
+                        const column = blockY * 3 + j;
+                        statement.push(fromCellToIndex(row, column, n));
                     }
                 }
                 clausesCount++;
@@ -45,26 +45,15 @@ const createClauses = () => {
             }
         }
     }
+
+    // In every spot if there is n, there cannot be m ( (9*8/2) * 81 = 2916 clauses)
     for (let row = 0; row < 9; row++) {
         for (let column = 0; column < 9; column++) {
-            // there is a number in cell (row, column)
-            {
-                const statement = [];
-                for (let n = 1; n < 10; n++) {
-                    statement.push(row * 9 * 9 + column * 9 + n);
+            for (let n = 1; n < 10; n++) {
+                for (let m = n + 1; m < 10; m++) {
+                    clausesCount++;
+                    data.push(2, -(fromCellToIndex(row, column, n)), -(fromCellToIndex(row, column, m)));
                 }
-                clausesCount++;
-                data.push(statement.length, ...statement);
-            }
-            // If there is n, there cannot be m
-            {
-                for (let n = 1; n < 10; n++) {
-                    for (let m = n + 1; m < 10; m++) {
-                        clausesCount++;
-                        data.push(2, -(row * 9 * 9 + column * 9 + n), -(row * 9 * 9 + column * 9 + m));
-                    }
-                }
-
             }
         }
     }
@@ -76,15 +65,22 @@ const createClauses = () => {
     }
 }
 
+// adds the clauses of the known numbers
 const addClauses = (values, data) => {
     for (let row = 0; row < 9; row++) {
         for (let column = 0; column < 9; column++) {
             const value = values[row][column];
             if (value !== 0) {
-                data.push(1, row * 9 * 9 + column * 9 + value);
+                data.push(1, fromCellToIndex(row, column, value));
             }
         }
     }
+}
+
+// the statement there is a number n in (row, column) has index:
+// index = row*9*9+column*9+n
+const fromCellToIndex = (row, column, n) => {
+    return row * 9 * 9 + column * 9 + n;
 }
 
 const fromIndexToCell = (index) => {
