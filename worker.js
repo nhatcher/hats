9a7a4b86e29c980b8ba7b07bc2e70c89c7294a2b
Nicolás Hatcher <nicolas@theuniverse.today>
2024-01-28 22:20:51 +0100

    UPDATE: Uses a webworker

---
+++ worker.js
@@ -0,0 +1,19 @@
+import __wbg_init, { solveSat } from './pkg/splr_wasm.js';
+
+async function init() {
+    await __wbg_init();
+}
+
+init();
+
+onmessage = (e) => {
+    console.log("Message received from main script");
+    const data = e.data[0];
+    const clausesCount = e.data[1];
+    const t0 = performance.now();
+    const result = JSON.parse(solveSat(data));
+    const t1 = performance.now();
+    result.time = t1 - t0;
+    result.clausesCount = clausesCount;
+    postMessage(result);
+};
